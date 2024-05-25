10bb1dd4d964e6adcb7280a79619677839289a99
Jan Graichen <jgraichen@altimos.de>
2024-05-25 14:23:55 +0200

    fix: Restore original export.py script
    Note: this does not work for STEP export of new assemblies

--- freecad/bin/export.py
+++ freecad/bin/export.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env fccmd
 # pylint: disable=missing-docstring,invalid-name
 # pyright: reportMissingImports=false, reportMissingModuleSource=false
 """
@@ -8,20 +8,6 @@ Export first root object to STL or STEP.
 import argparse
 import os
 import os.path
-import sys
-from pprint import pprint
-
-for path in [
-    "~/.local/lib/freecad/lib",
-    "~/.local/share/freecad/Mod",
-    "/usr/local/lib/freecad/lib",
-    "/usr/local/share/freecad/Mod",
-    "/usr/lib/freecad/lib",
-    "/usr/share/freecad/Mod",
-]:
-    path = os.path.expanduser(path)
-    if os.path.exists(path):
-        sys.path.append(path)
 
 # pylint: disable=import-error,wrong-import-position
 import FreeCAD  # noqa: E402
@@ -32,27 +18,17 @@ FORMAT_MESH = ["obj", "stl"]
 FORMAT_PART = ["step", "wrl"]
 
 
-def export(output: str, src: str | None = None):
-    _, ext = os.path.splitext(output)
-    fmt = ext.strip(".")
+def export(output: str):
+    _, fmt = os.path.splitext(output)
 
     os.makedirs(os.path.dirname(output), exist_ok=True)
 
-    pprint((output, fmt))
-
-    if src:
-        FreeCAD.open(src)
     doc = FreeCAD.activeDocument()
 
     # Only export all part features objects from the first root element
     root = doc.RootObjects[0]
 
-    pprint((root, root.Label, root.TypeId))
-
-    # Mesh is easy to export; pass root object and be done
-    if fmt.lower() in FORMAT_MESH:
-        Mesh.export([root], output)
-        return
+    # pprint((root, root.Label, root.TypeId))
 
     if root.isDerivedFrom("Part::Feature"):
         # Root is a part body: We can directly export that
@@ -60,27 +36,20 @@ def export(output: str, src: str | None = None):
 
     elif root.isDerivedFrom("App::Part"):
         # Root is an assembly: Scan for all individual parts
-        objects = list()
-        objects.extend(doc.findObjects("Part::Feature"))
-        objects.extend(doc.findObjects("App::Link"))
+        objects = doc.findObjects("Part::Feature")
         objects = [o for o in objects if root in o.InListRecursive]
 
-    pprint(objects)
+    # pprint([(o, o.Label, o.TypeId) for o in objects])
 
-    Part.export(objects, output)
+    if fmt.lower() in FORMAT_MESH:
+        Mesh.export([root], output)
+    else:
+        Part.export(objects, output)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("src", nargs=1)
-    parser.add_argument("output", nargs=1)
-    args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("trash", nargs="*")
+parser.add_argument("--pass", dest="output", nargs=1)
+args = parser.parse_args()
 
-    export(args.output[0], args.src[0])
-else:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("trash", nargs="*")
-    parser.add_argument("--pass", dest="output", nargs=1)
-    args = parser.parse_args()
-
-    export(args.output[0])
+export(args.output[0])
